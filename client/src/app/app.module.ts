ec303311c16e33a5686fb4d67a1af746d96b06a2
Daniel Oliveira - Sedet <daniel.araujo@sedet.ce.gov.br>
2022-06-29 23:13:05 -0300

    ajustando novas rotas

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,9 +11,11 @@ import { HomeComponent } from './home/home.component';
 import { CadastroParceiroComponent } from './cadastro-parceiro/cadastro-parceiro.component';
 import { CadastroNomadComponent } from './cadastro-nomad/cadastro-nomad.component';
 import { O_que_eComponent } from './o_que_e/o_que_e.component';
+import { ManualComponent } from './manual/manual.component';
+import { SeloComponent } from './selo/selo.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [						
     AppComponent,
     CadastroMunicipioComponent,
       HeaderComponent,
@@ -22,7 +24,9 @@ import { O_que_eComponent } from './o_que_e/o_que_e.component';
       HomeComponent,
       CadastroParceiroComponent,
       CadastroNomadComponent,
-      O_que_eComponent
+      O_que_eComponent,
+      ManualComponent,
+      SeloComponent
    ],
   imports: [
     BrowserModule,
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { ManualComponent } from './manual/manual.component';
 import { CadastroNomadComponent } from './cadastro-nomad/cadastro-nomad.component';
 import { CadastroParceiroComponent } from './cadastro-parceiro/cadastro-parceiro.component';
 import { HomeComponent } from './home/home.component';
@@ -8,10 +9,13 @@ import { AppComponent } from './app.component';
 import { CadastroMunicipioComponent } from './cadastro-municipio/cadastro-municipio.component';
 import { Ceara_prontoComponent } from './ceara_pronto/ceara_pronto.component';
 import { O_que_eComponent } from './o_que_e/o_que_e.component';
+import { SeloComponent } from './selo/selo.component';
 
 const APP_ROUTES: Routes = [
   { path: 'ceara_pronto', component: Ceara_prontoComponent },
   { path: 'o_que_e', component: O_que_eComponent },
+  { path: 'manual', component: ManualComponent },
+  { path: 'selo', component: SeloComponent },
   { path: 'cadastro_municipio', component: CadastroMunicipioComponent },
   { path: 'cadastro_parceiro', component: CadastroParceiroComponent },
   { path: 'cadastro_nomad', component: CadastroNomadComponent },
