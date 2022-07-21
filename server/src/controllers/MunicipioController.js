8f750fcb4d0557d2567f2d7bc3fb01f814cf5cd2
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2022-07-21 14:49:18 -0300

    pesquisa uma regiao

--- server/src/controllers/MunicipioController.js
+++ server/src/controllers/MunicipioController.js
@@ -9,7 +9,20 @@ class MunicipioController {
         catch (error){
             return res.status(500).json(error.message)
         }     
+    }
 
+    static async pegaUmaRegiao(req, res){
+        const { id } = req.params
+        try{
+            const umaRegiao = await database.Regiao.findOne( {
+                where: {
+                    id: Number(id)
+                }
+            })            
+            return res.status(200).json(umaRegiao)
+        }catch (error){
+            return res.status(500).json(error.message)
+        }
     }
 }
 
