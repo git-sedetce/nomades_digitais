345db619d572ce41319d0431176d0bfdd9dc5d29
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2022-07-25 16:23:39 -0300

    corrigindo endereco de api

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://api-digitalnomads.ce.gov.br//',
+  url: 'http://api-digitalnomads.sedet.ce.gov.br//',
   _url: 'http://172.20.19.165:2225/',
 };
