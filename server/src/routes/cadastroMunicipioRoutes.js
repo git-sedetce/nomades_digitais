55e732b5eb6b486dc109a59b6e7b44ff5faeb677
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2022-08-17 15:36:05 -0300

    corrigindo rota

--- server/src/routes/cadastroMunicipioRoutes.js
+++ server/src/routes/cadastroMunicipioRoutes.js
@@ -18,7 +18,7 @@ const router = Router()
 router.get('/parceiroMunicipio', CadastroMunicipioController.pegarMunicipioParceiro)
 router.get('/parceiroMunicipio/:id', CadastroMunicipioController.municipioParceiroById)
 router.post('/parceiroMunicipio', CadastroMunicipioController.cadastraMunicipioParceiro)
-router.post('/anexos', upload.array('files'), CadastroMunicipioController.anexosMunicipioParceiro)
+router.post('/anexosMunicipio', upload.array('files'), CadastroMunicipioController.anexosMunicipioParceiro)
 
 
 module.exports = router
