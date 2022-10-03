4a126c7c5df8cb4264800328d91bcbc5f324012a
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2022-10-03 14:57:10 -0300

    corrigindo relacionamento

--- server/src/models/cadastra_parceiros.js
+++ server/src/models/cadastra_parceiros.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      cadastra_parceiros.hasMany(models.anexos, { foreignKey: 'user_id'})
     }
   }
   cadastra_parceiros.init({
