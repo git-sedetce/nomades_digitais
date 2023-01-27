ef27ab78a12513767c3b5b74f1d69c7d62dfd293
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2023-01-27 11:18:31 -0300

    alterar o protocolo de segurança

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://api-digitalnomads.sedet.ce.gov.br/',
-  _url: 'http://172.20.19.165:2225/',
+  url: 'https://api-digitalnomads.sedet.ce.gov.br/',
+  _url: 'https://172.20.19.165:2225/',
 };
