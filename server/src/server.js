f850f8b16d7c2dbb1cf041a4e4e5e42f57b81348
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2023-01-27 11:19:19 -0300

    configurando o CORS

--- server/src/server.js
+++ server/src/server.js
@@ -1,60 +1,28 @@
 const  express = require('express')
-const bodyParser = require("body-parser")
+const cors = require('cors');
 const routes = require('./routes')
-const cors = require("cors");
-
-
-var whitelist = [
-  'http://localhost:4290',
-  'http://digitalnomads.ce.gov.br',
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Acesso negado à API " + origin));
-    }
-  },
-  optionsSuccessStatus: 200, // For legacy browser support
-  methods: "GET, PUT, POST, DELETE",
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-};
-/*var corsOptions = {
-  origin: 'http://localhost:4290',
-  optionsSuccessStatus:200,
-};*/
 
 const app = express()
 const port = 2225
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+var corsOptions = {
+  origin: ['https://digitalnomads.ce.gov.br', 'http://localhost:4290', 'https://localhost:4290'],
+  optionsSuccessStatus:200,
+  methods: "GET, PUT, POST, DELETE",
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Access-Control-Allow-Origin"],
+};
 app.use(cors(corsOptions))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 routes(app)
-
-var whitelist = [
-    "http://localhost:4290",
-    "http://localhost",
-  ]; //process.env.WHITE_LIST;
-
-var corsOptions = {
-    origin: function (origin, callback) {
-        callback(null, true);
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Acesso negado à API " + origin));
-      }
-    },
-    optionsSuccessStatus: 200, // For legacy browser support
-    methods: "GET, PUT, POST, DELETE",
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-  };
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  
-  app.use(cors());
 
 app.listen(port, () => console.log(`O Servidor está rodando`))
 
