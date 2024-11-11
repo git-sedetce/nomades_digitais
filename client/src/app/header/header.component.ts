af03a54daa5ade6f219a6c2a57ae507708682e0c
danielAraujo1406 <daniel.araujo@sedet.ce.gov.br>
2024-11-11 14:12:33 -0300

    verificar se está logado

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, DoCheck } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -10,17 +11,30 @@ export class HeaderComponent implements DoCheck {
   user_name: any;
   profile: any;
 
-  constructor() { }
+  constructor(
+    private serviceUser: UserService,
+  ) { }
 
   ngDoCheck() {
+    this.isLogged();
   }
 
   isLogged(){
+    const token = localStorage.getItem('token');
+    // console.log('token', token)
+    if(token){
+      this.authenticated = true
+      // console.log('authenticated', this.authenticated)
+      const loginUsers:any =  JSON.parse(atob(token!.split('.')[1]))
+      this.user_name = loginUsers._user_name;
+      this.profile = loginUsers._profile_id
+      // console.log('loginUsers', loginUsers)
+    }
 
   }
 
   logout(){
-
+    this.serviceUser.logout()
   }
 
 }
