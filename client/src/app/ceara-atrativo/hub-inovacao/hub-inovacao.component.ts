aa69409d5fa4da469fd2ad3af51673e3ad19d567
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2025-09-03 16:08:37 -0300

    concluindo sessão ceará atrativo

--- client/src/app/ceara-atrativo/hub-inovacao/hub-inovacao.component.ts
+++ client/src/app/ceara-atrativo/hub-inovacao/hub-inovacao.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hub-inovacao',
   templateUrl: './hub-inovacao.component.html',
   styleUrls: ['./hub-inovacao.component.css']
 })
-export class HubInovacaoComponent {
+export class HubInovacaoComponent implements OnInit {
+
+  viewHome: boolean = true;
+  viewIncubadoras: boolean = false;
+  viewAceleradoras: boolean = false;
+  viewParquesTecnologicos: boolean = false;
+
+  ngOnInit(): void {
+
+  }
+
+  verIncubadora():void {
+    this.viewHome = false;
+    this.viewIncubadoras = true;
+    this.viewAceleradoras = false;
+    this.viewParquesTecnologicos = false;
+  }
+
+  verAceleradora():void {
+    this.viewHome = false;
+    this.viewIncubadoras = false;
+    this.viewAceleradoras = true;
+    this.viewParquesTecnologicos = false;
+  }
+
+  verParque():void {
+    this.viewHome = false;
+    this.viewIncubadoras = false;
+    this.viewAceleradoras = false;
+    this.viewParquesTecnologicos = true;
+  }
+
+  return():void {
+    this.viewHome = true;
+    this.viewIncubadoras = false;
+    this.viewAceleradoras = false;
+    this.viewParquesTecnologicos = false;
+  }
 
 }
