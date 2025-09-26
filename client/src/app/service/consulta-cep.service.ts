d17db83309cd777d182c79bdf71ce315a2a06f12
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2025-09-26 10:54:10 -0300

    reorganizar consulta cep

--- client/src/app/service/consulta-cep.service.ts
+++ client/src/app/service/consulta-cep.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultaCepService {
 
+  private url = 'https://viacep.com.br/ws';
+
   constructor(private httpClient: HttpClient) { }
 
-  buscar(cep: string){
-    return this.httpClient.get(`https://viacep.com.br/ws/${cep}/json`)
+  buscar(cep: string): Observable<any> {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep !== '' && /^[0-9]{8}$/.test(cep)) {
+      return this.httpClient.get(`${this.url}/${cep}/json`);
+    }
+
+    throw new Error('CEP inválido');
   }
 }
