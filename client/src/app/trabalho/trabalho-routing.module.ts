7e02221c663b027cdd222cf148219ffe9c359d10
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2025-10-08 10:48:20 -0300

    linkando os componentes de trabalho

--- client/src/app/trabalho/trabalho-routing.module.ts
+++ client/src/app/trabalho/trabalho-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CadastraVagaComponent } from './cadastra-vaga/cadastra-vaga.component';
+import { VerVagaComponent } from './ver-vaga/ver-vaga.component';
+import { VerTalentosComponent } from './ver-talentos/ver-talentos.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'cadastra',
+    component: CadastraVagaComponent,
+  },
+  {
+    path: 'vervaga',
+    component: VerVagaComponent,
+  },
+  {
+    path: 'vertalento',
+    component: VerTalentosComponent,
   },
 ];
 
--- client/src/app/trabalho/trabalho.module.ts
+++ client/src/app/trabalho/trabalho.module.ts
@@ -8,11 +8,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgxMaskModule } from 'ngx-mask';
+import { CadastraVagaComponent } from './cadastra-vaga/cadastra-vaga.component';
+import { VerVagaComponent } from './ver-vaga/ver-vaga.component';
+import { VerTalentosComponent } from './ver-talentos/ver-talentos.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CadastraVagaComponent,
+    VerVagaComponent,
+    VerTalentosComponent
   ],
   imports: [
     CommonModule,
